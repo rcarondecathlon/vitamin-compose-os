a8c204dfaee15f8412bdc5b242936fa1af7d34b0
Romain Caron <romain.caron@decathlon.com>
2022-07-15 14:21:13 +0200

    continue

--- quantities/src/main/java/com/decathlon/vitamin/compose/quantities/VitaminQuantities.kt
+++ quantities/src/main/java/com/decathlon/vitamin/compose/quantities/VitaminQuantities.kt
@@ -1,16 +1,20 @@
 package com.decathlon.vitamin.compose.quantities
 
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.decathlon.vitamin.compose.foundation.VitaminTheme
-import com.decathlon.vitamin.compose.textinputs.VitaminTextInputs
 
 object VitaminQuantities {
 
@@ -40,9 +44,11 @@ object VitaminQuantities {
         onSubtractClicked: () -> Unit,
         onValueChange: (String) -> Unit
     ) {
-        LazyRow(modifier = modifier
-            .height(48.dp)
-            .width(154.dp)) {
+        LazyRow(
+            modifier = modifier
+                .height(48.dp)
+                .width(156.dp)
+        ) {
             item {
                 Button(
                     onClick = onSubtractClicked,
@@ -55,17 +61,51 @@ object VitaminQuantities {
                         contentColor = colors.actionTextColor,
                         disabledBackgroundColor = colors.actionBackgroundColor.copy(alpha = 0.38f),
                         disabledContentColor = colors.actionTextColor.copy(alpha = 0.38f)
+                    ),
+                    shape = RoundedCornerShape(
+                        topStart = 4.dp,
+                        topEnd = 0.dp,
+                        bottomEnd = 0.dp,
+                        bottomStart = 4.dp
+                    ),
+                    border = BorderStroke(
+                        width = 1.dp,
+                        color = colors.textBoxBorderColor
                     )
-                    ) {
+                ) {
                     Text(text = "-")
                 }
             }
             item {
-                TextField(
-                    value = value,
-                    onValueChange = onValueChange,
-                    enabled = addEnabled || subtractEnabled
-                )
+                Box(
+                    modifier = Modifier
+                        .width(60.dp)
+                        .height(48.dp)
+                        .background(
+                            color = if (!addEnabled && !subtractEnabled) colors.textBoxBackgroundColor.copy(
+                                alpha = 0.38f
+                            ) else colors.textBoxBackgroundColor
+                        )
+                ) {
+                    TextField(
+                        value = value,
+                        modifier = Modifier
+                            .align(Alignment.Center)
+                            .matchParentSize(),
+                        onValueChange = onValueChange,
+                        enabled = addEnabled || subtractEnabled,
+                        textStyle = LocalTextStyle.current.copy(textAlign = TextAlign.Center),
+                        colors = TextFieldDefaults.textFieldColors(
+                            backgroundColor = if (!addEnabled && !subtractEnabled) colors.textBoxBackgroundColor.copy(
+                                alpha = 0.38f
+                            ) else colors.textBoxBackgroundColor,
+                            focusedIndicatorColor = Color.Transparent,
+                            unfocusedIndicatorColor = Color.Transparent,
+                            disabledIndicatorColor = Color.Transparent,
+                            errorIndicatorColor = Color.Transparent
+                        )
+                    )
+                }
             }
             item {
                 Button(
@@ -73,7 +113,23 @@ object VitaminQuantities {
                     modifier = Modifier
                         .height(48.dp)
                         .width(48.dp),
-                    enabled = addEnabled
+                    enabled = subtractEnabled,
+                    colors = ButtonDefaults.buttonColors(
+                        backgroundColor = colors.actionBackgroundColor,
+                        contentColor = colors.actionTextColor,
+                        disabledBackgroundColor = colors.actionBackgroundColor.copy(alpha = 0.38f),
+                        disabledContentColor = colors.actionTextColor.copy(alpha = 0.38f)
+                    ),
+                    shape = RoundedCornerShape(
+                        topStart = 0.dp,
+                        topEnd = 4.dp,
+                        bottomEnd = 4.dp,
+                        bottomStart = 0.dp
+                    ),
+                    border = BorderStroke(
+                        width = 1.dp,
+                        color = colors.textBoxBorderColor
+                    )
                 ) {
                     Text(text = "+")
                 }
@@ -83,12 +139,17 @@ object VitaminQuantities {
 
 }
 
-@Preview(showBackground = true,
-backgroundColor = 0xffffffff)
+@Preview(
+    showBackground = true,
+    backgroundColor = 0xffffff00
+)
 @Composable
 fun PreviewQuantities() {
     VitaminTheme {
-        LazyColumn() {
+        LazyColumn(
+            contentPadding = PaddingValues(5.dp),
+            verticalArrangement = Arrangement.spacedBy(5.dp)
+        ) {
             item {
                 VitaminQuantities.Primary(
                     value = "0",
